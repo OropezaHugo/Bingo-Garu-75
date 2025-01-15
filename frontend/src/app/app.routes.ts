71e847cc51e612138a1e57fea5a2b8e2c23f0a69
AngheloZambrana <anghelozambrana2005@gmail.com>
2025-01-15 09:59:03 -0400

    feat/HomePageComponente and fixing stylization

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,16 +1,18 @@
 import { Routes } from '@angular/router';
-import {LobbyPage} from "./pages/lobby-page/lobby-page";
 import {SerialsBasePageComponent} from "./serials/serials-base-page/serials-base-page.component";
 import {SerialsBasePageContentComponent} from "./serials/serials-base-page-content/serials-base-page-content.component";
 import {GenerateSerialContentComponent} from "./serials/generate-serial-content/generate-serial-content.component";
 import {AttachSerialContentComponent} from "./serials/attach-serial-content/attach-serial-content.component";
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LobbyPage } from './pages/lobby-page/lobby-page';
 
 export const routes: Routes = [
-    {path: 'lobby', component: LobbyPage},
     { path: "serials", component: SerialsBasePageComponent, children: [
         { path: "", component: SerialsBasePageContentComponent },
         { path: "generate", component: GenerateSerialContentComponent },
         { path: "attach", component: AttachSerialContentComponent },
-        ]
-    }
-]
+        ], pathMatch: 'full'},
+    { path: "", component: HomePageComponent, pathMatch: 'full'},
+    {path: "patterns", component: LobbyPage},
+    {path: "instructions", component: LobbyPage}
+];
--- frontend/src/app/pages/home-page/home-page.component.ts
+++ frontend/src/app/pages/home-page/home-page.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
-import {RouterOutlet} from "@angular/router";
+import {RouterLink, RouterOutlet} from "@angular/router";
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-home-page',
-    imports: [HeaderComponent, FooterComponent, RouterOutlet],
+    imports: [HeaderComponent,
+      FooterComponent,
+      RouterOutlet,
+      MatIconModule,
+      RouterLink],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
