20dfa7c41cfd0a50d3e94e7e588bca5fac7f311d
AngheloZambrana <anghelozambrana2005@gmail.com>
2025-01-22 21:35:18 -0400

    feat/adding-functionality-footer

--- frontend/src/app/shared/footer/footer.component.ts
+++ frontend/src/app/shared/footer/footer.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
 import { TextButtonComponent } from '../buttons/text-button/text-button.component';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { Footer1DialogComponent } from '../dialogs/footer-1-dialog/footer-1-dialog.component';
+import { Footer2DialogComponent } from '../dialogs/footer-2-dialog/footer-2-dialog.component';
 
 @Component({
   selector: 'app-footer',
-  imports: [TextButtonComponent],
+  imports: [TextButtonComponent,
+    MatDialogModule
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+  constructor(private dialog: MatDialog) {}
 
+  openAboutUsDialog(): void {
+    this.dialog.open(Footer1DialogComponent, {
+      width: '500px'
+    });
+  }
+
+  openContactDialog(): void {
+    this.dialog.open(Footer2DialogComponent, {
+      width: '500px'
+    });
+  }
 }
