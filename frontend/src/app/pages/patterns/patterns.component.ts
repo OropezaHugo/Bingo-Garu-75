cca4b01851de1fd2c35bc156157c7c3ab94c6b96
AngheloZambrana <anghelozambrana2005@gmail.com>
2025-02-01 21:25:27 -0400

    fix pattern page

--- frontend/src/app/pages/patterns/patterns.component.ts
+++ frontend/src/app/pages/patterns/patterns.component.ts
@@ -1,16 +1,80 @@
-import { Component } from '@angular/core';
-import {
-  PatternListComponentComponent
-} from '../../sections/pattern-section/pattern-list-component/pattern-list-component.component';
+import { Component, inject, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { PatternService } from '../../core/services/pattern.service';
+import { GameService } from '../../core/services/game.service';
+import { Pattern, EmptyPattern, EmptyPatternData } from '../../core/models/add-pattern-dialog-data';
+import { NewPatternDialogComponent } from '../../sections/pattern-section/new-pattern-dialog/new-pattern-dialog.component';
+import { PatternCardComponent } from "../../shared/pattern-card/pattern-card.component";
+import { MatIcon } from '@angular/material/icon';
+import { MatFormField, MatLabel, MatSuffix } from '@angular/material/form-field';
+import { MatButton, MatIconButton } from '@angular/material/button';
+import { MatTooltip } from '@angular/material/tooltip';
+import { MatInput } from '@angular/material/input';
 
 @Component({
   selector: 'app-patterns',
   imports: [
-    PatternListComponentComponent
-  ],
+    MatFormField,
+    MatIcon,
+    MatIconButton,
+    MatInput,
+    MatLabel,
+    MatSuffix,
+    MatTooltip,
+    PatternCardComponent
+    ],
   templateUrl: './patterns.component.html',
-  styleUrl: './patterns.component.scss'
+  styleUrl: './patterns.component.scss',
 })
-export class PatternsComponent {
+export class PatternsComponent implements OnInit {
+  patternService = inject(PatternService);
+  gameService = inject(GameService);
+  readonly dialog = inject(MatDialog);
 
+  ngOnInit() {
+    this.patternService.getPatterns();
+  }
+
+  openEditPatternDialog(pattern: Pattern): void {
+    const dialogRef = this.dialog.open(NewPatternDialogComponent, {
+      data: pattern,
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result !== undefined && result.pattern !== EmptyPattern) {
+        this.patternService.updatePattern(result);
+      } else {
+        this.patternService.getPatterns();
+      }
+    });
+  }
+
+  openCreatePatternDialog(): void {
+    const dialogRef = this.dialog.open(NewPatternDialogComponent, {
+      data: EmptyPattern,
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result !== undefined && result.pattern !== EmptyPattern) {
+        this.patternService.postPattern(result);
+        this.patternService.getPatterns();
+        EmptyPattern.patternName = "";
+        EmptyPattern.patternMatrix = EmptyPatternData;
+      }
+    });
+  }
+
+  filterPatterns(event: Event) {
+    this.patternService.getPatterns((event.target as HTMLInputElement).value);
+  }
+
+  addToGame(pattern: Pattern) {
+    if (!this.gameService.gamePatterns().includes(pattern)) {
+      this.gameService.addPatternToActualGame(pattern.id);
+    }
+  }
+
+  deletePattern(pattern: Pattern) {
+    this.patternService.deletePattern(pattern.id);
+  }
 }
