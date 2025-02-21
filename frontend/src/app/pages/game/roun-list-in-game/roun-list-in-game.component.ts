38584529dffa1cecec09ba97880ed0ac6f900e1a
Hugo Oropeza <oropezahugo35@gmail.com>
2025-02-21 10:26:01 -0400

    feat: see winners

--- frontend/src/app/pages/game/roun-list-in-game/roun-list-in-game.component.ts
+++ frontend/src/app/pages/game/roun-list-in-game/roun-list-in-game.component.ts
@@ -5,6 +5,8 @@ import {RoundService} from '../../../core/services/round.service';
 import {MatIconButton} from '@angular/material/button';
 import {MatIcon} from '@angular/material/icon';
 import {RoundPatternInGameComponent} from '../round-pattern-in-game/round-pattern-in-game.component';
+import {MatBottomSheet} from '@angular/material/bottom-sheet';
+import {PatternWinnersSheetComponent} from '../pattern-winners-sheet/pattern-winners-sheet.component';
 
 @Component({
   selector: 'app-roun-list-in-game',
@@ -20,6 +22,8 @@ export class RounListInGameComponent implements OnInit, OnChanges{
   round = input.required<Round>()
   roundPatterns: RoundPatternInfo[] = []
   roundService = inject(RoundService)
+  private _bottomSheet = inject(MatBottomSheet);
+
   ngOnInit() {
     this.roundService.getPatternsInfoByRoundId(this.round().id!).subscribe({
       next: result => {
@@ -40,4 +44,19 @@ export class RounListInGameComponent implements OnInit, OnChanges{
     })
   }
 
+  checkWinners(pattern: RoundPatternInfo) {
+    this.roundService.getRoundsById(this.round().id!).subscribe({
+      next: result => {
+        this._bottomSheet.open(PatternWinnersSheetComponent, {
+          data: {
+            cards: this.roundService.gameService.gameCards().filter(card =>
+              this.roundService.isBingoValidAndNotPassed(pattern.patternMatrix, card.contentMatrix, result.raffleNumbers)
+            ),
+            pattern: pattern
+          }
+        });
+      }
+    })
+
+  }
 }
