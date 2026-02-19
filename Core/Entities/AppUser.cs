167d224738517f1c9f3f99271af318c3c872e582
Hugo Oropeza <oropezahugo35@gmail.com>
2026-02-19 11:29:55 -0400

    solve comments

--- Core/Entities/AppUser.cs
+++ Core/Entities/AppUser.cs
@@ -6,6 +6,6 @@ public class AppUser: BaseEntity
 {
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
-    public string? Email { get; set; }
+    public string Email { get; set; }
     public string Role { get; set; }
 }
